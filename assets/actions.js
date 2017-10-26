a5ba043f5de7b45d7d2c0e2350589b9e228e839b
Madhur Karnik <madhur.karnik@gmail.com>
2017-10-26 21:36:23 +0530

    Modify index to add sections and design according to specs.

--- assets/actions.js
+++ assets/actions.js
@@ -5,14 +5,15 @@ var tStart = 0 // Start transition from top
   , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
 
 $(document).ready(function() {
-    $(document).scroll(function() {
+    $(this).scrollTop(0);
+    /*$(document).scroll(function() {
         var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
         p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
         var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
         $("body").css('background-color', 'rgb(' + cBg.join(',') +')');
-    });
+    });*/
 	
-	$("#txt-name").change(function() {
+    $("#txt-name").change(function() {
         $("#ig-name").removeClass("has-error");
     });
 
@@ -30,9 +31,9 @@ function sendInviteRequest() {
 }
 
 function validateInputs() {
-	isValid = true;
+    isValid = true;
     var name, email, mobile, gender;
-	$(".err-mark").hide();
+    $(".err-mark").hide();
     name = $("#txt-name").val().trim();
     email = $("#txt-email").val().trim();
     mobile = $("#txt-mobile").val().trim();
@@ -41,18 +42,18 @@ function validateInputs() {
     emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(name === '' || name.length < 3) {
         isValid = false;
-		$("#ig-name").addClass("has-error");
+        $("#ig-name").addClass("has-error");
     } else if(mobile === '' || isNaN(Number(mobile)) || mobile.length !== 10) {
         isValid = false;
-		$("#ig-mobile").addClass("has-error");
+        $("#ig-mobile").addClass("has-error");
     } else if(email === '' || !(emailRegEx.test(email))) {
         isValid = false;
-		$("#ig-email").addClass("has-error");
+        $("#ig-email").addClass("has-error");
     } else if(typeof gender === "undefined" || (gender !== 'M' && gender !== 'F')) {
         isValid = false;
-		$("#err-gender").show();
+        $("#err-gender").show();
     } else if(typeof tnc === "undefined") {
-		isValid = false;
+        isValid = false;
         $("#err-tnc").show();
     }
 
