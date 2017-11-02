ac64d2d4fcfc08c454ee93a37a6efcca703be20d
Madhur Karnik <32730905+madhur-karnik@users.noreply.github.com>
2017-11-03 01:06:50 +0530

    Comment out scroll to change bg color function

--- assets/actions.js
+++ assets/actions.js
@@ -1,17 +1,20 @@
-var tStart = 0 // Start transition from top
-  , tEnd = 3277   // End at 500px
-  , cStart = [204, 0, 0]// [255, 99, 71]  // Gold
-  , cEnd = [0, 0, 0]// [66, 66, 132]   // Lime
-  , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
+
 
 $(document).ready(function() {
     $(this).scrollTop(0);
-    /*$(document).scroll(function() {
+    /*var tStart = 0 // Start transition from top
+    , tEnd = 2000   // End at 500px
+    , cStart = [255, 0, 100]// [255, 99, 71]  // Gold
+    , cEnd = [0, 0, 255]// [66, 66, 132]   // Lime
+    , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
+
+    $(document).scroll(function() {
         var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
         p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
         var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
         $("body").css('background-color', 'rgb(' + cBg.join(',') +')');
     });*/
+	
 	
     $("#txt-name").change(function() {
         $("#ig-name").removeClass("has-error");
